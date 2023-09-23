3cc5164508d5cd1f0c4cacb33ae1e3a16847e9f9
henrymon342 <henrymon342@gmail.com>
2023-09-23 16:52:59 -0400

    puerto corregido

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const http = require('./app');
 
-const port = 3000;
+const port = process.env.PORT || 3000
 
 http.listen(port, () => {
     console.log('server is running on port', port);
 });
+
+// const express = require("express")
+// const app = express()
+
+// const port = process.env.PORT || 3000
